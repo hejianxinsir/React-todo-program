178becd472ceb851e597609311dbdf4420e8c036
hejianxin <hejianxinsir@gmail.com>
2020-05-07 17:21:50 +0800

    添加 TodoItem 的样式

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ export default class App extends Component{
             onSubmit={this.addTodo.bind(this)}
           />
         </div>
-        <ol>
+        <ol className="todoList">
           {todos}
         </ol>
       </div>
