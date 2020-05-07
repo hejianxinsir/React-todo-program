3fd494f202ee8a889cfae8a7b78052b2eb7546fa
hejianxin <hejianxinsir@gmail.com>
2020-05-07 18:16:48 +0800

    新建 leancloud.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,7 @@ import 'normalize.css';
 import './css/reset.css';
 import * as localStore from './localStorage.js';
 
-import AV from 'leancloud-storage';
 
-AV.init({
-  appId: "wvmYu2gHtvKQFLP80IFJIsmO-gzGzoHsz",
-  appKey: "S8t4Rw8RBK0WmBnzNrV2Ln9d",
-  serverURL: "https://wvmyu2gh.lc-cn-n1-shared.com"
-});
 
 
 export default class App extends Component{
---
+++ src/leancloud.js
@@ -0,0 +1,9 @@
+import AV from 'leancloud-storage';
+
+AV.init({
+  appId: "wvmYu2gHtvKQFLP80IFJIsmO-gzGzoHsz",
+  appKey: "S8t4Rw8RBK0WmBnzNrV2Ln9d",
+  serverURL: "https://wvmyu2gh.lc-cn-n1-shared.com"
+});
+
+export default AV;
